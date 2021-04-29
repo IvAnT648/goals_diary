cd17e9334c7e5947e191015dc0e03e3ca3c73df8
Ivan Antyukhin <antyukhin@miura.tech>
2021-04-30 00:12:17 +0300

    Login screen was began

--- lib/src/view/screens/login/screen.dart
+++ lib/src/view/screens/login/screen.dart
@@ -1,13 +1,41 @@
 
 import 'package:flutter/material.dart';
+import 'package:goals_diary/common/res/styles.dart';
+
+import '../../../../generated/l10n.dart';
+import '../../components/all.dart';
 
 class LoginScreen extends StatelessWidget {
   static String id = '/login';
+  static const double _titlePadding = 90;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      backgroundColor: AppColors.background,
+      body: SafeArea(
+        top: true,
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.symmetric(
+            horizontal: 58,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: _titlePadding),
+              Text(
+                S.current.screenLoginTitle,
+                style: TextStyles.h1,
+              ),
+              SizedBox(height: _titlePadding),
+              AppTextField(
+                label: S.current.screenLoginNicknameLabel,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
