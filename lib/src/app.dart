83cf4de4d4a7b6127f5bb8970443fcef0c45e16f
Ivan Antyukhin <antyukhin@miura.tech>
2021-04-30 00:11:07 +0300

    App entry point updated

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 import '../common/res/styles.dart';
@@ -8,42 +9,47 @@ import 'view/navigation.dart';
 class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Goals Diary',
-      theme: ThemeData(
-        primaryColor: AppColors.primary,
-        primaryColorLight: AppColors.primary[10],
-        primaryColorDark: AppColors.primary[-10],
-        accentColor: AppColors.accent,
-        colorScheme: ColorScheme(
-          primary: AppColors.primary,
-          primaryVariant: AppColors.primary[-10],
-          secondary: AppColors.secondary,
-          secondaryVariant: AppColors.secondary[-10],
-          surface: AppColors.surface,
-          background: AppColors.background,
-          error: AppColors.negative,
-          onPrimary: AppColors.onPrimary,
-          onSecondary: AppColors.onSecondary,
-          onSurface: AppColors.gray[-10],
-          onBackground: AppColors.gray[-10],
-          onError: AppColors.onPrimary,
-          brightness: Brightness.light,
-        ),
-        pageTransitionsTheme: PageTransitionsTheme(builders: {
-          TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
-          TargetPlatform.android: CupertinoPageTransitionsBuilder(),
-        }),
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
       ),
-      localizationsDelegates: [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: S.delegate.supportedLocales,
-      initialRoute: ScreenNavigationProvider.initialRoute,
-      routes: ScreenNavigationProvider.getRoutes(),
+      child: MaterialApp(
+        title: 'Goals Diary',
+        theme: ThemeData(
+          primaryColor: AppColors.primary,
+          primaryColorLight: AppColors.primary[10],
+          primaryColorDark: AppColors.primary[-10],
+          accentColor: AppColors.accent,
+          colorScheme: ColorScheme(
+            primary: AppColors.primary,
+            primaryVariant: AppColors.primary[-10],
+            secondary: AppColors.secondary,
+            secondaryVariant: AppColors.secondary[-10],
+            surface: AppColors.surface,
+            background: AppColors.background,
+            error: AppColors.negative,
+            onPrimary: AppColors.onPrimary,
+            onSecondary: AppColors.onSecondary,
+            onSurface: AppColors.onSurface,
+            onBackground: AppColors.onBackground,
+            onError: AppColors.onPrimary,
+            brightness: Brightness.light,
+          ),
+          pageTransitionsTheme: PageTransitionsTheme(builders: {
+            TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+            TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+          }),
+        ),
+        localizationsDelegates: [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
+        initialRoute: ScreenNavigationProvider.initialRoute,
+        routes: ScreenNavigationProvider.getRoutes(),
+      ),
     );
   }
 }
