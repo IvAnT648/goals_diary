a6b35ef88dd64c49bbad2439290fea9de1af9189
Ivan Antyukhin <antyukhin@miura.tech>
2021-05-25 16:12:33 +0300

    User info model created

--- lib/src/domain/models.dart
+++ lib/src/domain/models.dart
@@ -1,2 +1,3 @@
 export 'models/goals.dart';
 export 'models/date_time.dart';
+export 'models/user.dart';
---
+++ lib/src/domain/models/user.dart
@@ -0,0 +1,99 @@
+
+class UserInfoDto {
+  final String nickname;
+  final String name;
+  final String? surname;
+  final String? info;
+  final String? motto;
+  final String? avatarUrl;
+
+  UserInfoDto({
+    required this.nickname,
+    required this.name,
+    this.surname,
+    this.info,
+    this.motto,
+    this.avatarUrl,
+  });
+
+  String get nicknameWithAt => '@$nickname';
+
+  String get fullName {
+    if (surname == null || surname!.isEmpty) {
+      return name;
+    }
+    return '$name $surname';
+  }
+
+  String get nameAbbreviation {
+    return (name.isNotEmpty ? name[0].toUpperCase() : '')
+        + (surname != null ? surname![0].toUpperCase() : '');
+  }
+
+  UserInfoDto copyWith({
+    String? name,
+    String? nickname,
+    String? info,
+    String? motto,
+    String? avatarUrl,
+  }) {
+    return UserInfoDto(
+      name: name ?? this.name,
+      nickname: nickname ?? this.nickname,
+      info: info ?? this.info,
+      motto: motto ?? this.motto,
+      avatarUrl: avatarUrl ?? this.avatarUrl,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'nickname': nickname,
+      'info': info,
+      'motto': motto,
+      'avatarUrl': avatarUrl,
+    };
+  }
+
+  factory UserInfoDto.fromMap(Map<String, dynamic> map) {
+    return UserInfoDto(
+      name: map['name'],
+      nickname: map['nickname'],
+      info: map['info'],
+      motto: map['motto'],
+      avatarUrl: map['avatarUrl'],
+    );
+  }
+
+  @override
+  String toString() {
+    return 'UserDto(name: $name'
+        ', nickname: $nickname'
+        ', info: $info'
+        ', motto: $motto'
+        ', avatarUrl: $avatarUrl'
+        ')';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is UserInfoDto &&
+        other.name == name &&
+        other.nickname == nickname &&
+        other.info == info &&
+        other.motto == motto &&
+        other.avatarUrl == avatarUrl;
+  }
+
+  @override
+  int get hashCode {
+    return name.hashCode ^
+        nickname.hashCode ^
+        info.hashCode ^
+        motto.hashCode ^
+        avatarUrl.hashCode;
+  }
+}
