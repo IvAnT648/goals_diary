6be01aeac6f7cb555b9a257ea68955ccefff1eaf
Ivan Antyukhin <antyukhin@miura.tech>
2021-05-28 01:08:30 +0300

    Get subscriptions usecase created

--- lib/src/domain/usecases.dart
+++ lib/src/domain/usecases.dart
@@ -2,3 +2,4 @@ export 'usecases/auth.dart';
 export 'usecases/my_goals.dart';
 export 'usecases/profile.dart';
 export 'usecases/get_posts.dart';
+export 'usecases/get_subscriptions.dart';
---
+++ lib/src/domain/usecases/get_subscriptions.dart
@@ -0,0 +1,62 @@
+
+import '../models/user.dart';
+
+abstract class GetSubscriptionsUseCase {
+  List<UserInfoDto> call();
+}
+
+class GetSubscriptionsUseCaseMock implements GetSubscriptionsUseCase {
+  final users = <UserInfoDto>[
+    UserInfoDto(
+      id: 0,
+      name: 'Bot',
+      nickname: 'bot1',
+    ),
+    UserInfoDto(
+      id: 0,
+      name: 'Bot',
+      surname: 'Ivanovich',
+      nickname: 'bot2',
+      avatarUrl: 'https://databytenitt.github.io/img/male.png',
+    ),
+    UserInfoDto(
+      id: 0,
+      name: 'Саша',
+      surname: 'Петрова',
+      nickname: 'sacsha_p',
+    ),
+    UserInfoDto(
+      id: 3,
+      name: 'Лиза',
+      surname: 'Иванова',
+      nickname: 'livanova88',
+      info: 'Программист 88 lvl',
+      motto: 'Не всякий кодер - программист',
+      avatarUrl: 'https://databytenitt.github.io/img/female.png',
+    ),
+    UserInfoDto(
+      id: 4,
+      name: 'Константин',
+      surname: 'Красильников',
+      nickname: 'kk_over',
+      info: 'Лыжник, у которого лыжи - едут!',
+      motto: 'Будь собой!',
+      avatarUrl: 'https://databytenitt.github.io/img/male.png',
+    ),
+    UserInfoDto(
+      id: 1,
+      name: 'Alexander',
+      surname: 'Ivanov',
+      nickname: 'alexander442',
+      info: 'Программист 80 lvl',
+      motto: 'Не всякий кодер - программист',
+      avatarUrl: 'https://databytenitt.github.io/img/male.png',
+    ),
+  ];
+
+  @override
+  List<UserInfoDto> call() {
+    return users;
+  }
+}
+
